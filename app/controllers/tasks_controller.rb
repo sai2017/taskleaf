99af39dee6978286d669b0e781eadc98c54d42b6
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-02-20 15:32:39 +0900

    create tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,20 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
+
+  private
+  
 end
