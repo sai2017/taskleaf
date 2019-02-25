6071fcee8c0c30d80f14d9bb6508effbdc4c4745
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-02-26 00:51:02 +0900

    add before action login_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_required
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  private
+
+  def login_required
+    redirect_to login_path if unless current_user
   end
 end
