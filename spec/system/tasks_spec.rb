1cbcd0f2e967aeb945daa46306137655c13e8e02
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-02-26 23:50:34 +0900

    overwriting let task_name

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -54,6 +54,7 @@ describe 'タスク管理機能', type: :system do
 
   describe '新規作成機能' do
     let(:login_user) { user_a }
+    let(:task_name) { '新規作成のテストを書く' }
 
     before do
       visit new_task_path
@@ -62,8 +63,6 @@ describe 'タスク管理機能', type: :system do
     end
 
     context '新規作成画面で名称を入力したとき' do
-      let(:task_name) { '新規作成のテストを書く' }
-
       it '正常に登録される' do
         expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
       end
