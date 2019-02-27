757bbb6079522d94bcedd796bb057cfb1ed1f686
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-02-27 18:16:02 +0900

    add ActionMailer

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,7 @@ class TasksController < ApplicationController
 
     if @task.save
       logger.debug "task: #{@task.attributes.inspect}"
+      TaskMailer.creation_email(@task).deliver_now
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
