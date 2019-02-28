0fe7bb95586b243ab10d0e9510caad0009bfa1cf
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-02-28 16:53:03 +0900

    change ajax:success to SJR

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    head :no_content
+    # head :no_content
     # redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
