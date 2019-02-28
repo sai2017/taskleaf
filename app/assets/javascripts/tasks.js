d9c3fe7e552ab0aaf6012b957a0b0a236baf0671
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-02-28 16:06:39 +0900

    add ajax

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,21 @@
-document.addEventlistener('turbolinks:load', function() {
-  document.querySelectorAll('td').forEach(function(td) {
-    td.addEventlistener('mouseover', function(e) {
-      e.currentTarget.style.backgroundColor = '#eff';
-    });
+// document.addEventlistener('turbolinks:load', function() {
+//   document.querySelectorAll('td').forEach(function(td) {
+//     td.addEventlistener('mouseover', function(e) {
+//       e.currentTarget.style.backgroundColor = '#eff';
+//     });
+//
+//     td.addEventlistener('mouseout', function(e) {
+//       e.currentTarget.style.backgroundColor = '';
+//     });
+//   });
+// });
 
-    td.addEventlistener('mouseout', function(e) {
-      e.currentTarget.style.backgroundColor = '';
+document.addEventListener('turbolinks:load', function() {
+  document.querySelectorAll('.delete').forEach(function(a) {
+    a.addEventListener('ajax:success', function() {
+      var td = a.parentNode;
+      var tr = td.parentNode;
+      tr.style.display = 'none';
     });
   });
 });
